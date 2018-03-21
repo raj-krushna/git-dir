d221bb34568e5f5b2bf26c927f3b0f175df7c3aa
krushna raj <iloninatarj@gmail.com>
2018-03-21 11:25:17 +0530

    new pirint statment is add for python.py

---
+++ python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+
+
+
+"""
+ksdfjlkaslka
+ajfjlasjflkasf
+"""
+
+print "never give up ..!!!"
+print "bring it on"
+print 'nataraj'
+PRINT "This file has been modifeid"
+print """ ljadslsalkasa f """
